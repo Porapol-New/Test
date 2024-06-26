4a3cc5d6fa9d717d80d4bc2a603817f042745135
Porapol-New <comideoaiu@gmail>
2024-06-26 22:51:44 +0700

    First

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    title: "Green Office",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Green Office"),
+      ),
+    ),
+  );
+  runApp(app);
+}
