1dcb95f93d17039f4bc69a0da5792c8b2121fe70
Porapol-New <comideoaiu@gmail.com>
2024-06-27 13:21:05 +0700

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
+     theme: ThemeData(primarySwatch: Colors.blueGrey),
     title: "Green Office",
     home: Scaffold(
       appBar: AppBar(
-        title: Text("Green Offices"),
+        title: Text(
+          "Green Office",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [],
+        // backgroundColor: Color.fromARGB(255, 71, 71, 71),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(20),
+              bottomLeft: Radius.circular(20)),
+        ),
       ),
+      body: Column(
+        
+      ),
+      
     ),
+    debugShowCheckedModeBanner: false,
+   
   );
   runApp(app);
 }
+// body:  Container(
+      //       color: Colors.black,
+      //       child: Row(
+      //         children: <Widget>[
+      //           Expanded(
+      //             child: Text('Demo', style: TextStyle(color: Colors.white),),
+      //           )
+      //         ],
+      //       ),
+      //     ),
