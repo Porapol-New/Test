4cfd29b370fb480de4d44a3e28a171a98d98802e
Porapol-New <comideoaiu@gmail.com>
2024-07-18 17:57:14 +0700

    Test 13

--- lib/Body3.dart
+++ lib/Body3.dart
@@ -15,8 +15,7 @@ class _Body3State extends State<Body3> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4')
+    _controller = VideoPlayerController.asset('assets/videos/Green-Office.mp4')
       ..initialize().then((_) {
         setState(() {});
       });
@@ -43,25 +42,36 @@ class _Body3State extends State<Body3> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Padding(padding: EdgeInsets.all(0)),
-        const Padding(padding: EdgeInsets.all(20)),
+        const Padding(padding: EdgeInsets.all(1)),
         Expanded(
           child: Column(
-            children: [
-              const Text(
+            children: const [
+              Text(
                 "การประหยัดพลังงาน",
                 style: TextStyle(fontSize: 50),
               ),
-              const Text(
-                "  ใช้อุปกรณ์ไฟฟ้าที่มีประสิทธิภาพสูง ปรับการใช้แสงไฟ เครื่องใช้ไฟฟ้า และระบบทำความร้อน/เย็นให้มีประสิทธิภาพสูงสุด และปิดไฟและอุปกรณ์เมื่อไม่ได้ใช้งาน",
+              Text(
+                "ใช้อุปกรณ์ไฟฟ้าที่มีประสิทธิภาพสูง ปรับการใช้แสงไฟ เครื่องใช้ไฟฟ้า และระบบทำความร้อน/เย็นให้มีประสิทธิภาพสูงสุด และปิดไฟและอุปกรณ์เมื่อไม่ได้ใช้งาน",
                 style: TextStyle(fontSize: 18),
                 textAlign: TextAlign.center,
               ),
-              if (_controller.value.isInitialized)
-                AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
+            ],
+          ),
+        ),
+        if (_controller.value.isInitialized)
+          Column(
+            children: [Padding(padding: EdgeInsets.only(right: 10)),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: SizedBox(
+                  width: 800, // ปรับความกว้างของวิดีโอที่นี่
+                  height: 500, // ปรับความสูงของวิดีโอที่นี่
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
                 ),
+              ),
               IconButton(
                 icon: Icon(
                   _isPlaying ? Icons.pause : Icons.play_arrow,
@@ -69,8 +79,7 @@ class _Body3State extends State<Body3> {
                 onPressed: _togglePlayPause,
               ),
             ],
-          ),
-        ),
+          )
       ],
     );
   }
